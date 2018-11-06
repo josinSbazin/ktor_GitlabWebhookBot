f174b34ba964a7bc8af4d5e6e3245f549441e5cf
rhanza <rhanza@bk.ru>
2018-11-06 19:41:45 +0300

    * comment problem code

--- src/data/mergeRequest/Changes.kt
+++ src/data/mergeRequest/Changes.kt
@@ -7,8 +7,8 @@ data class Changes(
     @field:SerializedName("updated_by_id")
     val updatedById: List<Long?> = emptyList(),
 
-    @field:SerializedName("updated_at")
-    val updatedAt: List<String?> = emptyList(),
+//    @field:SerializedName("updated_at")
+//    val updatedAt: List<Long?> = emptyList(),
 
     @field:SerializedName("labels")
     val labels: Labels? = null
